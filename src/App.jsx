cc9ae7772c7d69c16daba0832d3c62913a03e11b
jesiel.borges.corp <jesiel.borges.corp@gmail.com>
2025-05-03 14:33:45 +0000

    Finalized Task taskManager

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,68 @@
+import Task from "./components/taskManager/Tasks";
+import AddTask from "./components/taskManager/AddTask";
+import { useEffect, useState } from "react";
+import Title from "./components/taskManager/Title";
 
-function App(){
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=3",
+        { method: "GET" }
+      );
+      const data = await response.json();
+      setTasks(data);
+    };
+
+    fetchTasks();
+  }, []);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onTaskDelete(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(tile, description) {
+    const newTask = {
+      id: tasks.length + 1,
+      title: tile,
+      description: description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
-  <div className="w-screen h-screen bg-gray-100 flex justify-center p-6">
-    <h1 className="text-red-500">Oi</h1>  
-  </div>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de tarefas</Title>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Task
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onTaskDelete={onTaskDelete}
+        />
+      </div>
+    </div>
   );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter,Route,RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import TaskPage from './components/taskManager/pages/TaskPage.jsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/task',
+    element: <TaskPage />,
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
